4a9783128e133a9f8e95d90825ce18726a2c34c8
mikeymellc <mikeymellc@gmail.com>
2025-09-29 13:41:14 -0400

    Delete src/pages/Shop.js

--- src/pages/Shop.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Welcome to MIKEYME</h1>
-      <p>Supplying quality, budget THCA products from our small farm.</p>
-    </div>
-  );
-}
