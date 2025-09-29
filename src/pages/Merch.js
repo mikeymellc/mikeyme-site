5afb857f51255d58844b2f4a939c948f02aab47a
mikeymellc <mikeymellc@gmail.com>
2025-09-29 12:49:08 -0400

    Create Merch.js

---
+++ src/pages/Merch.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to MIKEYME</h1>
+      <p>Supplying quality, budget THCA products from our small farm.</p>
+    </div>
+  );
+}
