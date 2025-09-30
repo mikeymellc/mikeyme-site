9f85d588fdbeb2aabe01ffa6c22a258216f3cf60
mikeymellc <mikeymellc@gmail.com>
2025-09-30 15:58:29 -0400

    Delete src/pages/Greenhouse.js

--- src/pages/Greenhouse.js
+++
@@ -1,7 +0,0 @@
-<p>
-  Our **Greenhouse THCA flower** is sun-grown with supplemental lighting,
-  striking the perfect balance between natural elements and controlled
-  cultivation. Using organic practices and reduced energy input, this method
-  provides a <strong>smaller carbon footprint</strong> while still delivering
-  consistent potency.
-</p>
