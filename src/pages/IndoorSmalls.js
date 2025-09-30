0e9ef8144ee4cd92f867f22998b6b070a9feb4bb
mikeymellc <mikeymellc@gmail.com>
2025-09-30 14:51:04 -0400

    Create IndoorSmalls.js

---
+++ src/pages/IndoorSmalls.js
@@ -0,0 +1,6 @@
+<p>
+  Our **Indoor Smalls** are trimmed from the same premium indoor harvests but
+  feature smaller buds at a more budget-friendly price. Grown with organic
+  methods in a controlled setting, these buds deliver quality and sustainability
+  without compromise.
+</p>
