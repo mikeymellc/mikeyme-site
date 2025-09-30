1ad641f0584c55e332c0cfebcf6b641c11eb5593
mikeymellc <mikeymellc@gmail.com>
2025-09-30 16:25:57 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+// src/components/Header.js
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="site-header">
+    <h1>MikeyMe</h1>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/Shop">Shop</a>
+      <a href="/Merch">Merch</a>
+      <a href="/About">About Us</a>
+      <a href="/Contact">Contact Us</a>
+    </nav>
+  </header>
+);
+
+export default Header;
