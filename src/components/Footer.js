4b6df049d3dafbba123d08e218f31f9a0750ff47
mikeymellc <mikeymellc@gmail.com>
2025-09-30 15:28:07 -0400

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from "react";
+import "./Footer.css"; // optional
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: "#111", color: "#2ecc71", padding: "20px", textAlign: "center" }}>
+      &copy; 2025 MikeyMe. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
