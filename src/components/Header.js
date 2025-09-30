72518da7ad9f4b6e1fe3f7875186f1430b5483ba
mikeymellc <mikeymellc@gmail.com>
2025-09-30 17:50:43 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HeaderGreenBlack.css"; // Make sure this path is correct
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/Merch">Merch</Link>
+          </li>
+          <li>
+            <Link to="/AboutUs">About Us</Link>
+          </li>
+          <li>
+            <Link to="/ContactUs">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
