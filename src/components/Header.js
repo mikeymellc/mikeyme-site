562036d650b1638823455be6948003b45d7bbe45
mikeymellc <mikeymellc@gmail.com>
2025-09-30 17:37:49 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HeaderGreenBlack.css"; // Make sure your green/black theme CSS is here
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-item"><Link to="/">Home</Link></li>
+          <li className="nav-item"><Link to="/Shop">Shop</Link></li>
+          <li className="nav-item"><Link to="/Merch">Merch</Link></li>
+          <li className="nav-item"><Link to="/AboutUs">About Us</Link></li>
+          <li className="nav-item"><Link to="/ContactUs">Contact Us</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
