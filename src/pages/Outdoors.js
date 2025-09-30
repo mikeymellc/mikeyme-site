0b264d43884fe71574f78ea1eff754d0e2416f26
mikeymellc <mikeymellc@gmail.com>
2025-09-30 14:52:10 -0400

    Create Outdoors.js

---
+++ src/pages/Outdoors.js
@@ -0,0 +1,6 @@
+<p>
+  Our **Outdoor THCA flower** is cultivated under the open sun using regenerative
+  farming techniques. With a focus on soil health and organic practices, this
+  approach leaves a <strong>minimal impact on the ecosystem</strong> while
+  producing natural, robust buds at the most budget-friendly price point.
+</p>
