3fc2e1499385c5d5df143afb47a9476678340f46
mikeymellc <mikeymellc@gmail.com>
2025-09-30 22:14:10 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import LiveResin from "./pages/LiveResin";
+import LiveRosin from "./pages/LiveRosin";
+import IndoorSmalls from "./pages/IndoorSmalls";
+import Indoor from "./pages/Indoor";
+import Greenhouse from "./pages/Greenhouse";
+import Outdoors from "./pages/Outdoors";
+import Merch from "./pages/Merch"; // ✅ Import Merch page
+
+// ✅ Import CartProvider
+import { CartProvider } from "./context/CartContext";
+
+function App() {
+  return (
+    <Router>
+      <CartProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/Merch" element={<Merch />} /> {/* ✅ Merch route added */}
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+
+          {/* Product pages */}
+          <Route path="/LiveResin" element={<LiveResin />} />
+          <Route path="/LiveRosin" element={<LiveRosin />} />
+          <Route path="/IndoorSmalls" element={<IndoorSmalls />} />
+          <Route path="/Indoor" element={<Indoor />} />
+          <Route path="/Greenhouse" element={<Greenhouse />} />
+          <Route path="/Outdoors" element={<Outdoors />} />
+        </Routes>
+      </CartProvider>
+    </Router>
+  );
+}
+
+export default App;
+            
