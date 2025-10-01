311e93d77d8ccf248a2af3fb8197f5669a107e24
mikeymellc <mikeymellc@gmail.com>
2025-09-30 23:54:06 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import AgeGate from "./components/AgeGate"; // ✅ import
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import LiveResin from "./pages/LiveResin";
+import LiveRosin from "./pages/LiveRosin";
+import IndoorSmalls from "./pages/IndoorSmalls";
+import Indoor from "./pages/Indoor";
+import Greenhouse from "./pages/Greenhouse";
+import Outdoors from "./pages/Outdoors";
+import Merch from "./pages/Merch";
+import { CartProvider } from "./context/CartContext";
+
+function App() {
+  const [ageConfirmed, setAgeConfirmed] = useState(false);
+
+  return (
+    <Router>
+      <CartProvider>
+        {!ageConfirmed && <AgeGate onConfirm={() => setAgeConfirmed(true)} />}
+        {ageConfirmed && (
+          <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Shop" element={<Shop />} />
+              <Route path="/Merch" element={<Merch />} />
+              <Route path="/AboutUs" element={<AboutUs />} />
+              <Route path="/ContactUs" element={<ContactUs />} />
+              <Route path="/LiveResin" element={<LiveResin />} />
+              <Route path="/LiveRosin" element={<LiveRosin />} />
+              <Route path="/IndoorSmalls" element={<IndoorSmalls />} />
+              <Route path="/Indoor" element={<Indoor />} />
+              <Route path="/Greenhouse" element={<Greenhouse />} />
+              <Route path="/Outdoors" element={<Outdoors />} />
+            </Routes>
+          </>
+        )}
+      </CartProvider>
+    </Router>
+  );
+}
+
+export default App;
+          
