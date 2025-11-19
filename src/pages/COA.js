26d9d083d4cf0ced9357dd8b93827f3ea4260099
mikeymellc <mikeymellc@gmail.com>
2025-11-19 15:59:40 -0600

    Create COA.js

---
+++ src/pages/COA.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./COA.css";
+
+export default function COA() {
+  return (
+    <div className="coa-page">
+      <h1>Certificate of Analysis (COA)</h1>
+      <p>Select a strain below to view or download its lab results.</p>
+
+      <ul className="coa-list">
+        <li><a href="/coa/sour-gas.pdf" target="_blank">Sour Gas</a></li>
+        <li><a href="/coa/blueberry-muffin.pdf" target="_blank">Blueberry Muffin</a></li>
+        <li><a href="/coa/cookies-2025.pdf" target="_blank">Cookies</a></li>
+        <li><a href="/coa/gassy-apple.pdf" target="_blank">Gassy Apple</a></li>
+      </ul>
+    </div>
+  );
+}
